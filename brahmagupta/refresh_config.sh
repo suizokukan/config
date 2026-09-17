53a0a6642fe2ab657962bea208ba7009006123d7
Xavier Faure <suizokukan@orange.fr>
2026-09-17 17:28:39 +0200

    modified:   refresh_config.sh

--- brahmagupta/refresh_config.sh
+++ brahmagupta/refresh_config.sh
@@ -1,6 +1,7 @@
 rm -rf .config
 cp -r ~/.config .
 rm -rf .config/mozilla/
+# base de données utilisateur de dconf, qui est le système de gestion des configurations de l'environnement de bureau GNOME (et d'autres environnements dérivés comme Cinnamon, MATE ou Budgie).
 rm -rf .config/dconf/
 
 rm -rf .emacs.d
